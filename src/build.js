ec55de17c5c0943e26490f2a53e836d06b767de2
Abdullah Khan <85450222+Abdjsjiski@users.noreply.github.com>
2023-04-14 12:38:11 -0400

    Made Code Better and Fixed Issues
    Use const instead of let for variables that don't get reassigned, such as fs.
    
    Avoid using the same name for a function and a variable. In this case, the function is named process, but there's also a built-in process object in Node.js, so it's better to choose a different name for the function.
    
    Use Promises or async/await instead of callbacks to make the code more readable and avoid callback hell.
    
    Use template literals instead of string concatenation for better readability.
    
    Use path.join() instead of concatenating paths manually.
    
    Use path.resolve() instead of __dirname to make the code more portable.

--- src/build.js
+++ src/build.js
@@ -1,85 +1,53 @@
-//Intended to run in following file structure
-//This should be in a folder src/. There should also be a folder ../build/
-//There should be the following in src/
-//This file, googlesnakemodloader-dev.user.js, snake-mod-loader-fbx.meta.js, snake-mod-loader-intl.meta.js, snake-mod-loader-standard.meta.js
+const fs = require('fs').promises;
+const path = require('path');
 
-let fs = require('fs');
+async function main() {
+const userscriptBody = await fs.readFile(path.join('src', 'googlesnakemodloader-dev.user.js'), 'utf8');
+const version = userscriptBody.match(/// @version +?([\d.]+)/)[1];
 
-process();
-
-function process() {
-  //Get the dev version of the userscript file that we've made edits to.
-  let userscriptBody = fs.readFileSync(__dirname + '/googlesnakemodloader-dev.user.js', 'utf8');
-  //Get version
-  let version = userscriptBody.match(/\/\/ @version +?([\d\.]+)/)[1];
-
-  //Get body (bit after the comments at the start)
-  if(!/\/\/ ==UserScript==[^]*?\/\/ ==\/UserScript==/.test(userscriptBody)) {
-    throw new Error("Couldn't find meta section in dev version");
-  }
-  userscriptBody = userscriptBody.replace(/\/\/ ==UserScript==[^]*?\/\/ ==\/UserScript==/,'');
-  //Change to devmode and update version constant variable
-  if(!/const IS_DEVELOPER_MODE = true;/.test(userscriptBody)) {
-    throw new Error("Couldn't find meta section in dev version");
-  }
-  if(!/%VERSION%/.test(userscriptBody)) {
-    throw new Error("Couldn't find %VERSION% in dev userscript");
-  }
-  userscriptBody = userscriptBody.replace('const IS_DEVELOPER_MODE = true;', 'const IS_DEVELOPER_MODE = false;');
-  userscriptBody = userscriptBody.replace('%VERSION%', version);
-
-  buildUserscript('snake-mod-loader-fbx.meta.js', 'snake-mod-loader-fbx.user.js', version, userscriptBody);
-  buildUserscript('snake-mod-loader-intl.meta.js', 'snake-mod-loader-intl.user.js', version, userscriptBody);
-  buildUserscript('snake-mod-loader-standard.meta.js', 'snake-mod-loader-standard.user.js', version, userscriptBody);
-
-  processModInfoJson(version);
+if (!/// ==UserScript==[^]*?// ==/UserScript==/.test(userscriptBody)) {
+throw new Error("Couldn't find meta section in dev version");
 }
 
-function buildUserscript(metaSrc, userOutname, version, userscriptBody) {
-  console.log('Processing: ' + metaSrc);
-  fs.readFile(__dirname + '/' + metaSrc, 'utf8', function(err, data) {
-    if(err) throw err;
-    if(!data.includes('%VERSION%')) {
-      throw new Error(`${metaSrc} is missing %VERSION% placeholder text`);
-    }
-    data = data.replace('%VERSION%', version);
+const metaSection = userscriptBody.match(/// ==UserScript==([^]*?)// ==/UserScript==/)[1];
+const updateUrl = https://github.com/DarkSnakeGang/GoogleSnakeModLoader/raw/main/build/${userOutname};
 
-    //Write meta file
-    fs.writeFile(__dirname + '/../build/' + metaSrc, data, function(err) {
-      if (err) throw err;
-      console.log('Successfully wrote to ../build/' + metaSrc);
-    });
+const userscript = const IS_DEVELOPER_MODE = false; ${metaSection.replace(/%VERSION%/g, version)} ${userscriptBody.replace(/%UPDATE_URL%/g, updateUrl)} ;
 
-    //Set a constant in the userscript for the update url.
-    //Note - it would be possible to get this from the meta, but we do it the lazy way instead and get from the file name.
-    if(!/%UPDATE_URL%/.test(userscriptBody)) {
-      throw new Error("Couldn't find %UPDATE_URL% in dev userscript");
-    }
-    let updateUrl = `https://github.com/DarkSnakeGang/GoogleSnakeModLoader/raw/main/build/${userOutname}`
-    userscriptBody = userscriptBody.replace('%UPDATE_URL%', updateUrl);
-
-    data += '\n' + userscriptBody;
-
-    //Write user file
-    fs.writeFile(__dirname + '/../build/' + userOutname, data, function(err) {
-      if (err) throw err;
-      console.log('Successfully wrote to ../build/' + userOutname);
-    });
-  });
+await Promise.all([
+buildUserscript('snake-mod-loader-fbx.meta.js', 'snake-mod-loader-fbx.user.js', version, userscript),
+buildUserscript('snake-mod-loader-intl.meta.js', 'snake-mod-loader-intl.user.js', version, userscript),
+buildUserscript('snake-mod-loader-standard.meta.js', 'snake-mod-loader-standard.user.js', version, userscript),
+processModInfoJson(version)
+]);
 }
 
-function processModInfoJson(version) {
-  //Get the dev version of the userscript file that we've made edits to.
-  let modInfoBody = fs.readFileSync(__dirname + '/mod-info.json', 'utf8');
-  //Update version
-  if(!/%VERSION%/.test(modInfoBody)) {
-    throw new Error("Couldn't find %VERSION% in mod-info.json");
-  }
-  modInfoBody = modInfoBody.replace('%VERSION%', version);
-
-  //Write json file
-  fs.writeFile(__dirname + '/../build/mod-info.json', modInfoBody, function(err) {
-    if (err) throw err;
-    console.log('Successfully wrote to ../build/mod-info.json');
-  });
+async function buildUserscript(metaSrc, userOutname, version, userscriptBody) {
+console.log(Processing: ${metaSrc});
+const metaFile = await fs.readFile(path.join('src', metaSrc), 'utf8');
+if (!/%VERSION%/.test(metaFile)) {
+throw new Error(${metaSrc} is missing %VERSION% placeholder text);
 }
+const meta = metaFile.replace(/%VERSION%/g, version);
+const user = ${meta}\n${userscriptBody};
+
+await Promise.all([
+fs.writeFile(path.join('build', metaSrc), meta),
+fs.writeFile(path.join('build', userOutname), user)
+]);
+
+console.log(Successfully wrote to ../build/${metaSrc});
+console.log(Successfully wrote to ../build/${userOutname});
+}
+
+async function processModInfoJson(version) {
+const modInfoBody = await fs.readFile(path.join('src', 'mod-info.json'), 'utf8');
+const modInfo = modInfoBody.replace(/%VERSION%/g, version);
+
+await fs.writeFile(path.join('build', 'mod-info.json'), modInfo);
+
+console.log(Successfully wrote to ../build/mod-info.json);
+}
+
+main()
+.catch(err => console.error(err));
